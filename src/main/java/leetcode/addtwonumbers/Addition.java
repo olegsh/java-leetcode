f151167ae86b25646b2c3045ee5a85d7e3aae5f2
Oleg S <oshnayderman@healthline.com>
2020-10-31 14:33:00 -0700

    some changes

--- src/main/java/leetcode/addtwonumbers/Addition.java
+++ src/main/java/leetcode/addtwonumbers/Addition.java
@@ -1,4 +1,4 @@
-package addtwonumbers;
+package leetcode.addtwonumbers;
 
 import structures.ListNode;
 
--- src/main/java/leetcode/structures/ListNode.java
+++ src/main/java/leetcode/structures/ListNode.java
@@ -1,4 +1,4 @@
-package structures;
+package leetcode.structures;
 
 public class ListNode<E> {
   private E data;
